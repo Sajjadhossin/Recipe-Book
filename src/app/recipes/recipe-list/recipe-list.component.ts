9b61e92589ad4ab9dd58fbc9bd08992b833d7cae
Sajjad Hossin <sajjadhossin.cse@gmail.com>
2024-03-14 04:01:24 +0600

    Work on Recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,26 @@
+import { Recipe } from './../recipe.model';
 import { Component } from '@angular/core';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [],
+  imports: [RecipeItemComponent],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.scss'
 })
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe('recipe name', 'recipe description' , 'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg'),
+    new Recipe('recipe name', 'recipe description' , 'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg'),
+    new Recipe('recipe name', 'recipe description' , 'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg'),
+    // {
+    //   name: 'recipe Name',
+    //   description: 'Recipe description',
+    //   imagePath: 'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-260nw-370298699.jpg'
+    // }
+  ];
 
 }
