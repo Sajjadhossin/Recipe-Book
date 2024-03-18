eee85315670473819f031daf5aef01547b3c4400
Sajjad Hossin <sajjadhossin.cse@gmail.com>
2024-03-19 02:05:32 +0600

    Shopping Edit component design

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { Ingredient } from '../shared/ingredient.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [ShoppingEditComponent],
+  imports: [CommonModule, ShoppingEditComponent],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.scss'
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient [] = [
+    new Ingredient ( 'Apple', 50),
+    new Ingredient ( 'Tomato', 20)
+  ];
 
 }
